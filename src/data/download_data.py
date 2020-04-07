3114ba211ab1d4532eb8c6d2f073adc0fe277841
Jason Bossert <jbossert3@gmail.com>
2020-04-07 13:22:15 -0600

    Read wikipedia CO counties table

--- src/data/download_data.py
+++ src/data/download_data.py
@@ -1,20 +1,30 @@
 import requests
-from bs4 import BeautifulSoup
+
+raw_root = "../../data/raw/"
+
 
 def download_wiki_counties():
-    pass
+    response = requests.get(
+        "https://en.wikipedia.org/wiki/List_of_counties_in_Colorado")
+    with open(raw_root + "state/counties_response.txt", 'w') as file:
+        file.write(response.text)
+
 
 def download_2016_precinct_shapefiles():
     pass
 
+
 def download_2018_precinct_shapefiles():
     pass
+
 
 def download_state_senate_districts():
     pass
 
+
 def download_state_house_districts():
     pass
+
 
 def download_federal_house_districts():
     pass
