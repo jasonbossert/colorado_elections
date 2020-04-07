8cf632ee95988702f47e7ef75942f98c5c42607c
Jason Bossert <jbossert3@gmail.com>
2020-04-07 10:15:32 -0600

    Add functions skeletons for precinct shapefiles

---
+++ src/data/download_data.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+def download_wiki_counties():
+    pass
+
+def download_2016_precinct_shapefiles():
+    pass
+
+def download_2018_precinct_shapefiles():
+    pass
+
+def download_state_senate_districts():
+    pass
+
+def download_state_house_districts():
+    pass
+
+def download_federal_house_districts():
+    pass
