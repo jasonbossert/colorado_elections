b6d9f9d2df48fa678dcf360b61efd4f653551862
Jason Bossert <jbossert3@gmail.com>
2020-04-10 07:18:14 -0600

    Move specific functions into submodules

--- src/data/clean_data.py
+++ src/data/clean_data.py
@@ -1,134 +1,37 @@
-import geopandas as gpd
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+from postgis.psycopg import register
+
 import clean_precincts as pre
+import clean_registration as reg
 
 
 if __name__ == '__main__':
     raw_root = "../../data/raw/"
     interim_root = "../../data/interim/"
 
-    """
-    CLEAN PRECINCT SHAPEFILES
+    # PRECINCT SHAPEFILES
+    print("Loading precinct shapefiles")
+    counties, precincts = pre.load_data(raw_root)
+    print("Cleaning precinct shapefiles")
+    clean_precincts = pre.clean_data(raw_root, counties, precincts)
 
-    """
+    # CLEAN VOTER REGISTRATION DATA
+    print("Loading voter registration data")
+    raw_registrations = reg.load_data(raw_root)
+    print("Cleaning voter registration data")
+    co_voters_table, voters_per_year = reg.clean_data(raw_registrations)
 
-    with open(raw_root + "state/counties_response.txt", 'r') as file:
-        counties_raw = file.read()
-    counties_df = pre.parse_wiki_counties_to_df(counties_raw)
-    counties = pre.clean_wiki_counties_df(counties_df)
-    counties['countynum'] = [str(z+1).zfill(2) for z in counties.index.values]
-    county_mapping = {}
-
-    precincts_2016 = (gpd.read_file(raw_root + "precincts/co_2016/")
-                         .to_crs(epsg=4269))
-    clean_2016 = pre.clean_precincts_2016(precincts_2016, counties)
-
-    precincts_2018 = (gpd.read_file(raw_root + "precincts/CO_precincts/")
-                         .to_crs(epsg=4269))
-    clean_2018 = pre.clean_precincts_2018(precincts_2018, counties)
-
-    state_senate = pre.read_and_fmt_state_vars(raw_root
-                                           + "state/state_senate_districts/")
-
-    state_house = pre.read_and_fmt_state_vars(raw_root
-                                          + "state/state_house_districts/")
-
-    federal_house = (gpd.read_file(raw_root + "state/tl_2016_us_cd115/")
-            .to_crs(epsg=4269)
-            .rename(columns=str.lower)
-            .rename(columns={'statefp': 'statefips',
-                             'cd115fp': 'congressional_district_115fips'})
-            .drop(columns=['geoid', 'namelsad', 'lsad', 'cdsessn',
-                           'mtfcc', 'funcstat', 'aland', 'awater',
-                           'intptlat', 'intptlon']))
-    co_fed_house = (federal_house[federal_house.statefips == '08']
-                    .reset_index(drop=True))
-
-    clean_2016 = pre.add_districts(clean_2016, state_senate,
-                                   state_house, co_fed_house)
-    clean_2018 = pre.add_districts(clean_2018, state_senate,
-                                   state_house, co_fed_house)
-
-    county_mapping = pre.get_county_name_to_number_mapping(raw_root)
-    clean_2016 = pre.update_county_vtdst(clean_2016, county_mapping)
-    clean_2018 = pre.update_county_vtdst(clean_2018, county_mapping)
-
-    """
-    OPEN DATABASE CONNECTION
-    """
-    import psycopg2
-    from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
-    from postgis.psycopg import register
-
+    # WRITE TO DB
     conn = psycopg2.connect(database="interim")
     conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
     register(conn)
     cur = conn.cursor()
 
-    """
-    WRITE COUNTIES TO DB
-    """
-    cur.execute("""DROP TABLE IF EXISTS colorado_counties""")
-    cur.execute("""
-        CREATE TABLE colorado_counties (
-            "id" INT PRIMARY KEY,
-            "county" TEXT NOT NULL UNIQUE,
-            "fips" TEXT NOT NULL UNIQUE,
-            "established" DATE NOT NULL,
-            "population" INT NOT NULL,
-            "area" FLOAT NOT NULL,
-            "countynum" TEXT NOT NULL
-        )""")
-    for _, row in counties.iterrows():
-        vals = [row.name] + list(row.iloc[[0, 1, 3, 4, 5, 6]])
-        cur.execute("""INSERT INTO colorado_counties
-                (id, county, fips, established, population, area, countynum)
-                VALUES (%s, %s, %s, %s, %s, %s, %s)""", vals)
-
-    """
-    WRITE PRECINCT SHAPEFILES TO DB
-    """
-
-    cur.execute("""DROP TABLE IF EXISTS precinct_shapefiles_2016""")
-    cur.execute("""
-        CREATE TABLE precinct_shapefiles_2016 (
-            "index" INT PRIMARY KEY,
-            "countyfips" TEXT NOT NULL,
-            "countyname" TEXT NOT NULL,
-            "countynum" TEXT NOT NULL,
-            "vtdst3" TEXT NOT NULL,
-            "vtdst5" TEXT NOT NULL UNIQUE,
-            "vtdst" TEXT NOT NULL UNIQUE,
-            "congressional_district_115fips" TEXT NOT NULL,
-            "state_legisture_upper" TEXT NOT NULL,
-            "state_legisture_lower" TEXT NOT NULL,
-            "geom" geometry)
-        """)
-    cur.execute("""DROP TABLE IF EXISTS precinct_shapefiles_2018""")
-    cur.execute("""
-        CREATE TABLE precinct_shapefiles_2018 (
-            like precinct_shapefiles_2016 including all)
-        """)
-
-    def write_precinct_shapefile_table(df, table_name):
-        srid_str = f"SRID={int(df.crs['init'][-4:])};"
-        for _, row in df.iterrows():
-            vars = ["countyfips", "countyname", "countynum", "vtdst3",
-                    "vtdst5", "vtdst", "congressional_district_115fips",
-                    "state_legisture_upper", "state_legisture_lower",
-                    "geometry"]
-            vals = [row.name] + list(row[vars].values)[:-1]
-            geom = srid_str + list(row[vars].values)[-1].wkt
-            vals.append(geom)
-            sql = """INSERT INTO """ + table_name + """
-                     (index, countyfips, countyname, countynum, vtdst3, vtdst5,
-                      vtdst, congressional_district_115fips,
-                      state_legisture_upper, state_legisture_lower, geom)
-                      VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s,
-                       %s, %s::geometry)"""
-            cur.execute(sql, vals)
-
-    write_precinct_shapefile_table(clean_2016, 'precinct_shapefiles_2016')
-    write_precinct_shapefile_table(clean_2018, 'precinct_shapefiles_2018')
+    print("Writing precinct shapefile data to database")
+    pre.write_data(cur, counties, clean_precincts)
+    print("Writing voter registration data to database")
+    reg.write_data(cur, co_voters_table, voters_per_year)
 
     cur.close()
     conn.close()
